3daa49bb23ea1e398cb2ade1fcb3715e3cc20d2d
Dan Halperin <dee.aye.en@gmail.com>
2019-10-27 17:13:26 +0200

    ready for implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import MyCard from "./MyCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="d-flex align-items-center">
+          <div
+            style={{ marginRight: "300px" }}
+            class="d-flex flex-column justify-content-center align-items-center"
+          >
+            <h1 class="mb-5" style={{ color: "white" }}>
+              React App
+              <img
+                src={logo}
+                width={"100px"}
+                className={"App-logo"}
+                alt="logo"
+              />
+            </h1>
+            <div class="card shadow-lg p-3">
+              <div class="d-flex flex-column">
+                <MyCard
+                  song={"God's Plan"}
+                  artist={"Drake"}
+                  image={
+                    "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                  }
+                />
+                <MyCard
+                  song={"One Dance"}
+                  artist={"Drake"}
+                  image={
+                    "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                  }
+                />
+                <MyCard
+                  song={"Hotline Bling"}
+                  artist={"Drake"}
+                  image={
+                    "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                  }
+                />
+              </div>
+            </div>
+            <h1 class="mt-5" style={{ color: "white" }}>
+              Before
+            </h1>
+          </div>
+          <div class="d-flex flex-column justify-content-center align-items-center">
+            <h1 class="mb-5" style={{ color: "white" }}>
+              React App
+              <img
+                src={logo}
+                width={"100px"}
+                className={"App-logo"}
+                alt="logo"
+              />
+            </h1>
+            <div class="card shadow-lg p-3">
+              <div class="d-flex flex-column">
+                <MyCard
+                  song={"God's Plan"}
+                  artist={"Drake"}
+                  image={
+                    "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                  }
+                />
+                <MyCard
+                  song={"One Dance"}
+                  artist={"Drake"}
+                  image={
+                    "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                  }
+                />
+                <MyCard
+                  song={"Hotline Bling"}
+                  artist={"Drake"}
+                  image={
+                    "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                  }
+                />
+              </div>
+            </div>
+            <h1 class="mt-5" style={{ color: "white" }}>
+              After
+            </h1>
+          </div>
+        </div>
       </header>
     </div>
   );
