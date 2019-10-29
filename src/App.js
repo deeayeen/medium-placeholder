2521d76db693cf6ded6dcf008f160b72f9e04f22
Dan Halperin <dee.aye.en@gmail.com>
2019-10-30 00:22:33 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -22,55 +22,51 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div class="d-flex align-items-center">
-            <div class="d-flex flex-column justify-content-center align-items-center">
-              <FadeIn>
-                <h1 class="mb-5" style={{ color: "white" }}>
-                  Placeholder Example
-                  <img
-                    src={logo}
-                    width={"100px"}
-                    className={"App-logo"}
-                    alt="logo"
-                  />
-                </h1>
-              </FadeIn>
-              <div class="card shadow-lg p-3">
-                <div class="d-flex flex-column">
-                  <div style={{ width: "317.75px" }}>
-                    {!this.state.finalLoad ? (
-                      <>
-                        <Placeholder />
-                        <Placeholder />
-                        <Placeholder />
-                      </>
-                    ) : (
-                      <FadeIn>
-                        <Card
-                          song={"God's Plan"}
-                          artist={"Drake"}
-                          image={
-                            "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
-                          }
-                        />
-                        <Card
-                          song={"One Dance"}
-                          artist={"Drake"}
-                          image={
-                            "https://upload.wikimedia.org/wikipedia/en/a/af/Drake_-_Views_cover.jpg"
-                          }
-                        />
-                        <Card
-                          song={"Hotline Bling"}
-                          artist={"Drake"}
-                          image={
-                            "https://www.billboard.com/files/styles/article_main_image/public/media/drake-hotline-bling-video-still-2015-billbaord-650.jpg"
-                          }
-                        />
-                      </FadeIn>
-                    )}
-                  </div>
-                </div>
+          <div class="d-flex flex-column justify-content-center align-items-center">
+            <FadeIn>
+              <h1 class="mb-5" style={{ color: "white" }}>
+                Placeholder Example
+                <img
+                  src={logo}
+                  width={"100px"}
+                  className={"App-logo"}
+                  alt="logo"
+                />
+              </h1>
+            </FadeIn>
+            <div class="card shadow-lg p-3">
+              <div style={{ width: "317.75px" }}>
+                {!this.state.finalLoad ? (
+                  <>
+                    <Placeholder />
+                    <Placeholder />
+                    <Placeholder />
+                  </>
+                ) : (
+                  <FadeIn>
+                    <Card
+                      song={"God's Plan"}
+                      artist={"Drake"}
+                      image={
+                        "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                      }
+                    />
+                    <Card
+                      song={"One Dance"}
+                      artist={"Drake"}
+                      image={
+                        "https://upload.wikimedia.org/wikipedia/en/a/af/Drake_-_Views_cover.jpg"
+                      }
+                    />
+                    <Card
+                      song={"Hotline Bling"}
+                      artist={"Drake"}
+                      image={
+                        "https://www.billboard.com/files/styles/article_main_image/public/media/drake-hotline-bling-video-still-2015-billbaord-650.jpg"
+                      }
+                    />
+                  </FadeIn>
+                )}
               </div>
             </div>
           </div>
